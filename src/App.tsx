c4827783d5d1a81c5190e19388996c6d3d47a30f
Andrés Fernando Torres Morán <atorres@applab.mx>
2024-02-26 20:40:58 -0600

    Pokeapi

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import './App.css';
 import Button from './components/Button';
-
+import {Pokemon} from './models/Pokemon';
+import {PokeApi} from './api/PokeApi';
+import PokemonComponent from './components/Pokemon';
 function App() {
-  const [contador, setContador] = React.useState(1);
-
-  function aumentar(){
-    console.log("Entra a la función aumentar");
-    setContador(contador + 1);
+  const [pokemonNumber, setPokemonNumber] = React.useState<string|undefined>(undefined);
+  const [pokemon,setPokemon]=React.useState<Pokemon|undefined>(undefined);
+  const [loading,setLoading]=React.useState<boolean>(false);
+  const [error,setError]=React.useState<string|undefined>(undefined);
+  function buscar(){
+    setLoading(true);
+    setError(undefined);
+    PokeApi.getPokemonById(pokemonNumber).then((response)=>{
+      setPokemon(response.data);
+      setLoading(false);
+    }).catch((error)=>{
+      console.log(error)
+      setLoading(false);
+      setError("Pokemon no encontrado");
+    });
   }
-
   return (
     <div className="App">
-      <p>Hello World {contador}</p>
-      <Button label="Incrementar" onClick={()=>aumentar()}></Button>
-      <Button label="Incrementar 2" color='green' onClick={()=>aumentar()}></Button>
-      <Button label="Incrementar 2" color='red' onClick={()=>aumentar()}></Button>
-
+      <p>Hello World {pokemonNumber}</p>
+      <input onChange={(event)=>{setPokemonNumber(event.target.value)}} className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" value={pokemonNumber}></input>
+      <Button label="Buscar" color='green' onClick={()=>buscar()}></Button>
+      {loading && <p>Cargando...</p>}
+      {(!loading && pokemon &&!error) && <>
+        <PokemonComponent pokemon={pokemon}></PokemonComponent>
+      </>}
+      {error && <p>{error}</p>}
     </div>
   );
 }
---
+++ src/api/PokeApi.tsx
@@ -0,0 +1,8 @@
+import {api} from './axiosConfig';
+
+export const PokeApi={
+    getPokemonById: async function(id:any){
+        return api.get(`/pokemon/${id}`)
+    }
+}
+
